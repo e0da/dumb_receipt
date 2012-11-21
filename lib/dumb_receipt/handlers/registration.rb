8c1a676e67f30783289214ed08a531503bc94f5a
Justin Force <justin.force@gmail.com>
2012-11-21 15:50:10 -0800

    Remove unnecessary comment
    Self-documenting code ftw!

--- lib/dumb_receipt/handlers/registration.rb
+++ lib/dumb_receipt/handlers/registration.rb
@@ -4,12 +4,6 @@ module DumbReceipt
   module Handlers
     class Registration < DumbReceipt::Handlers::Base
 
-      # "registration"
-      #
-      # If the client sets a parameter called 'fail', fail authentication.
-      # Otherwise, succeed. This has no effect on the client's ability to access
-      # data; it's just a mechanism for testing registration behavior in clients.
-      #
       post '/registration' do
         content_type :json
         if params[:fail]

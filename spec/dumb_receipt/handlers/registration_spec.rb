d6c7c379888bbc26ee7f5b6398f68411e70a1633
Justin Force <justin.force@gmail.com>
2012-11-21 17:12:44 -0800

    Get rid of pointless describe wrapper in Registration spec

--- spec/dumb_receipt/handlers/registration_spec.rb
+++ spec/dumb_receipt/handlers/registration_spec.rb
@@ -10,21 +10,17 @@ module DumbReceipt
         DumbReceipt::Handlers::Registration
       end
 
-      describe 'POST' do
+      describe 'POST /registration' do
+        it 'responds with success' do
+          post '/registration'
+          last_response.status.should be 200
+          last_response.headers['Content-Type'].should match %r[application/json]
+        end
 
-        describe '/registration' do
-          it 'responds with success' do
-            post '/registration'
-            last_response.status.should be 200
-            last_response.headers['Content-Type'].should match %r[application/json]
-          end
-
-          it 'fails if you pass a fail parameter' do
-            post '/registration', 'fail' => 'yes'
-            last_response.status.should be 400
-            last_response.headers['Content-Type'].should match %r[application/json]
-          end
-
+        it 'fails if you pass a fail parameter' do
+          post '/registration', 'fail' => 'yes'
+          last_response.status.should be 400
+          last_response.headers['Content-Type'].should match %r[application/json]
         end
       end
     end

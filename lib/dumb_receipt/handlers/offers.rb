e41855f86520da87ec6903ab8f5b40937fcf8451
Justin Force <justin.force@gmail.com>
2012-11-21 17:05:55 -0800

    Do /offers/redeem

--- lib/dumb_receipt/handlers/offers.rb
+++ lib/dumb_receipt/handlers/offers.rb
@@ -10,10 +10,30 @@ module DumbReceipt
       end
 
       post '/offers/read' do
+        content_type :json
+        read_result
+      end
+
+      post '/offers/redeem' do
+        content_type :json
+        redeem_result
+      end
+
+      private
+
+      def read_result
         if params[:fail]
-          [400, failure('read', 'offers_not_redeemed')]
+          [400, failure('read', 'offers_not_marked_as_read')]
         else
           [200]
+        end
+      end
+
+      def redeem_result
+        if params[:fail]
+          [400, failure('redeem', 'offer_not_redeemed')]
+        else
+          { offer: data['offers'][0] }.to_json
         end
       end
     end
